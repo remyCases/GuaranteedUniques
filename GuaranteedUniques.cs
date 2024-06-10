96a6ff8fab33a5f6e5c76663da6160423e52846b
remyCases <remy.cases@gmail.com>
2024-06-10 13:39:22 +0200

    [Major] change the enemies loot and remove the possibility of unique loot for non boss

--- GuaranteedUniques.cs
+++ GuaranteedUniques.cs
@@ -7,50 +7,74 @@ using ModShardLauncher.Mods;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace GuaranteedUniques;
 public class GuaranteedUniques : Mod
 {
     public override string Author => "zizani";
     public override string Name => "GuaranteedUniques";
-    public override string Description => "I've heard that some powerful enemies own unique gears. Defeat them and claim these !";
-    public override string Version => "0.1.0.0";
+    public override string Description => "A more reliable way to farm unique weapons and armors.";
+    public override string Version => "1.0.0.0";
     public override string TargetVersion => "0.8.2.10";
 
     public override void PatchMod()
     {
-        Dictionary<string, (string, string[])> map_items = new()
+        Dictionary<string, string[]> map_items = new()
         {
-            { "W_Light", ("o_skeleton_guardianSword", new string[] { "Relict Shield" }) },
-            { "W_Medium", ("o_bandit_ringleader_shield", new string[] { "Crow Heater Shield", "Joust Shield", "Uroboros Shield" }) },
-            { "W_Heavy", ("o_skeleton_guardianMace", new string[] { "Guardian Shield", "Sun Shield" }) },
-            { "Head_Light", ("o_bandit_paymaster", new string[] { "Hired Blade Cowl" }) },
-            { "Head_Medium", ("o_bandit_ringleader_2hsword", new string[] { "Decorated Barbute" }) },
-            { "Head_Heavy", ("o_proselyte_brander", new string[] { "Joust Topfhelm", "Joust Bascinet", "Pigfaced Bascinet", "Decorated Sallet", "Mastercrafted Sallet" }) },
-            { "Head_Magic", ("o_necromancer_wraithbinder", new string[] { "Hermit Circlet" }) },
-            { "Chest_Light", ("o_bandit_magehunter", new string[] { "Royal Ranger Gambeson" }) },
-            { "Chest_Heavy", ("o_proselyte_juggernaut", new string[] { "Joust Armor", "Ceremonial Cuirass", "Vagabond Knight Armor" }) },
-            { "Chest_Magic", ("o_ghast_elder", new string[] { "Druid Robe" }) },
-            { "Legs_Medium", ("o_revenant", new string[] { "Engraved Boots" }) },
-            { "Legs_Heavy", ("", new string[] { "Sardar Boots" }) },
-            { "Ring_Light", ("o_proselyte_wormbearer", new string[] { "Hermit Ring" }) },
-            { "Back_Light", ("o_proselyte_anmara", new string[] { "Azure Cape", "Occult Cloak" }) },
-            { "sword", ("o_bandit_duelist", new string[] { "Fancy Sword", "Engraved Sword", "Jarl Blade", "Royal Sword", "Relict Blade", "Guard Broadsword", "Decorated Saber", "Ancient Scimitar" }) },
-            { "axe", ("", new string[] { "Gilded Axe", "Feudal Axe" }) },
-            { "mace", ("o_bandit_raubritter_flail", new string[] { "Decorated Flanged Mace", "Decorated Warhammer" }) },
-            { "dagger", ("o_bandit_freak", new string[] { "Decorated Dagger" }) },
-            { "2hsword", ("o_bandit_raubritter_2hsword", new string[] { "Ornate Greatsword", "Espadon" }) },
-            { "spear", ("o_skeleton_guardianSpear", new string[] { "Faceless Spear" }) },
-            { "2haxe", ("o_skeleton_guardianAxe", new string[] { "Decorated Voulge", "Ornate Longaxe" }) },
-            { "2hmace", ("o_bandit_raubritter_2hmace", new string[] { "Exquisite Grandmace", "Relict Polehammer" }) },
-            { "bow", ("o_bandit_longbowman", new string[] { "Decorated Longbow", "Relic Bow" }) },
-            { "crossbow", ("o_bandit_huntmaster", new string[] { "Guard Crossbow" }) },
-            { "2hStaff", ("o_bandit_warlock", new string[] { "Hermit Staff", "Necromancer Staff", "Vampiric Staff", "Orient Staff", "Relict Staff" }) },
+            { "W_Light", new string[] { "Relict Shield" } },
+            { "W_Medium", new string[] { "Crow Heater Shield", "Joust Shield", "Uroboros Shield" } },
+            { "W_Heavy", new string[] { "Guardian Shield", "Sun Shield" } },
+            { "Head_Light", new string[] { "Hired Blade Cowl" } },
+            { "Head_Medium", new string[] { "Decorated Barbute" } },
+            { "Head_Heavy", new string[] { "Joust Topfhelm", "Joust Bascinet", "Pigfaced Bascinet", "Decorated Sallet", "Mastercrafted Sallet" } },
+            { "Head_Magic", new string[] { "Hermit Circlet" } },
+            { "Chest_Light", new string[] { "Royal Ranger Gambeson" } },
+            { "Chest_Heavy", new string[] { "Joust Armor", "Ceremonial Cuirass", "Vagabond Knight Armor" } },
+            { "Chest_Magic", new string[] { "Druid Robe" } },
+            { "Legs_Medium", new string[] { "Engraved Boots" } },
+            { "Legs_Heavy", new string[] { "Sardar Boots" } },
+            { "Ring_Light", new string[] { "Hermit Ring" } },
+            { "Back_Light", new string[] { "Azure Cape", "Occult Cloak" } },
+            { "sword", new string[] { "Fancy Sword", "Engraved Sword", "Jarl Blade", "Royal Sword", "Relict Blade", "Guard Broadsword", "Decorated Saber", "Ancient Scimitar" } },
+            { "axe", new string[] { "Gilded Axe", "Feudal Axe" } },
+            { "mace", new string[] { "Decorated Flanged Mace", "Decorated Warhammer" } },
+            { "dagger", new string[] { "Decorated Dagger" } },
+            { "2hsword", new string[] { "Ornate Greatsword", "Espadon" } },
+            { "spear", new string[] { "Faceless Spear" } },
+            { "2haxe", new string[] { "Decorated Voulge", "Ornate Longaxe" } },
+            { "2hmace", new string[] { "Exquisite Grandmace", "Relict Polehammer" } },
+            { "bow", new string[] { "Decorated Longbow", "Relic Bow" } },
+            { "crossbow", new string[] { "Guard Crossbow" } },
+            { "2hStaff", new string[] { "Hermit Staff", "Necromancer Staff", "Vampiric Staff", "Orient Staff", "Relict Staff" } },
         };
 
-        foreach (KeyValuePair<string, (string, string[])> entry in map_items)
+        Dictionary<string, string[]> enemies_loot = new()
         {
-            if (entry.Value.Item1 == "") continue;
+            { "o_bandit_huntmaster", new string[] { "crossbow" } },
+            { "o_bandit_freak", new string[] { "dagger", "Head_Light" } },
+            { "o_bandit_paymaster", new string[] { "2hsword", "Head_Light", "Chest_Light", "Back_Light" } },
+            { "o_bandit_ringleader_shield", new string[] { "sword", "Head_Medium", "Chest_Light", "Legs_Medium", "W_Medium" } },
+            { "o_bandit_ringleader_2hsword", new string[] { "2hsword", "Head_Medium", "Chest_Light", "Legs_Medium" } },
+            { "o_bandit_raubritter_2hsword", new string[] { "2hsword", "Head_Heavy", "Chest_Heavy", "Legs_Heavy" } },
+            { "o_bandit_raubritter_2hmace", new string[] { "2hmace", "Head_Heavy", "Chest_Heavy", "Legs_Heavy" } },
+            { "o_bandit_raubritter_flail", new string[] { "mace", "Head_Heavy", "Chest_Heavy", "Legs_Heavy", "W_Medium" } },
+            { "o_skeleton_guardianSword", new string[] { "sword", "Head_Heavy", "Chest_Heavy", "Legs_Heavy", "W_Light" } },
+            { "o_skeleton_guardianMace", new string[] { "mace", "Head_Heavy", "Chest_Heavy", "Legs_Heavy", "W_Heavy" } },
+            { "o_skeleton_guardianSpear", new string[] { "spear", "Head_Heavy", "Chest_Heavy", "Legs_Heavy" } },
+            { "o_skeleton_guardianAxe", new string[] { "2haxe", "Head_Heavy", "Chest_Heavy", "Legs_Heavy" } },
+            { "o_revenant", new string[] { "2hsword", "Head_Medium", "Chest_Heavy", "Legs_Medium" } },
+            { "o_necromancer_wraithbinder", new string[] { "2hStaff", "Head_Magic", "Chest_Magic", "Ring_Light", } },
+            { "o_ghast_elder", new string[] { "2hStaff", "Head_Magic", "Chest_Magic", "Ring_Light", } },
+            { "o_proselyte_brander", new string[] { "Head_Heavy" } },
+            { "o_proselyte_wormbearer", new string[] { "Ring_Light", } },
+            { "o_proselyte_anmara", new string[] { "axe", "bow", "Ring_Light", "Back_Light" } },
+            { "o_proselyte_juggernaut", new string[] { "2hmace", "Head_Medium", "Chest_Heavy", } },
+        };
+
+        foreach (KeyValuePair<string, string[]> entry in enemies_loot)
+        {
+            string[] loot = entry.Value.SelectMany(x => map_items[x]).ToArray();
             Msl.AddLootTable(
                 lootTableID: entry.Key,
                 guaranteedItems: new ItemsTable(
@@ -62,15 +86,15 @@ public class GuaranteedUniques : Mod
                 randomLootMax: 1,
                 emptyWeight: 0,
                 randomItemsTable: new RandomItemsTable(
-                    listItems: entry.Value.Item2,
-                    listRarity: Enumerable.Repeat(6, entry.Value.Item2.Length).ToArray(),
-                    listDurability: Enumerable.Repeat(100, entry.Value.Item2.Length).ToArray(),
-                    listWeight: Enumerable.Repeat(1, entry.Value.Item2.Length).ToArray()
+                    listItems: loot,
+                    listRarity: Enumerable.Repeat(6, loot.Length).ToArray(),
+                    listDurability: Enumerable.Repeat(100, loot.Length).ToArray(),
+                    listWeight: Enumerable.Repeat(1, loot.Length).ToArray()
                 )
             );
 
             Msl.AddReferenceTable(
-                nameObject: entry.Value.Item1,
+                nameObject: entry.Key,
                 table: entry.Key
             );
         }
